ee5e7d0cc02af36e0f0ac6b82fbc27e82841ffad
YoushaaMurhij <yosha.morheg@gmail.com>
2023-03-11 16:06:22 +0300

    fix: update submission path

--- tools/submit.sh
+++ tools/submit.sh
@@ -11,8 +11,8 @@ echo "Running inference script on ${split} set"
 python3 tools/inference.py --method $method --description $description --split $split --weight_path $weight --account $account --author $author; 
 
 echo "Compressing ${split} results for submission ..."
-tar czvf /home/user/STrajNet/Waymo_Dataset/inference/${method}_${split}.tar.gz  \
-      -C /home/user/STrajNet//Waymo_Dataset/inference/${split} .
+tar czvf /home/user/ofmpnet/Waymo_Dataset/inference/torch/${method}_${split}.tar.gz  \
+      -C /home/user/ofmpnet/Waymo_Dataset/inference/torch/${split} .
 echo "Deleting raw prediction protos ..."
-rm -r /home/user/STrajNet//Waymo_Dataset/inference/${split}
+rm -r /home/user/ofmpnet/Waymo_Dataset/inference/torch/${split}
 echo "Compression is done. Good luck!"
